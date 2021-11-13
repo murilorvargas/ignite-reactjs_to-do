e98350681c8802e1b4eb96bb72ebf09b2fc6f046
unknown <muriraubach@gmail.com>
2021-11-13 19:07:51 -0300

    Feat: add a new task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,22 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle.trim() === '') {
+      return
+    } else {
+      const newTask = {
+        id: new Date().getTime(),
+        title: newTaskTitle,
+        isComplete: false,
+      }
+      setTasks(oldState => [...oldState, newTask])
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
   }
 
   return (
